1b68585d2830c4b0c0c2f06cee295230fc062785
thiago_Silva_Ext <thiago.augusto.ext@dasa.com.br>
2022-10-19 10:56:11 -0300

    descompactando e somando casos pela chave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Imports:
+import re
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -6,6 +7,8 @@ from apache_beam.options.pipeline_options import PipelineOptions
 #Setting options:
 pipeline_options = PipelineOptions(argc=None)
 pipeline = beam.Pipeline(options=pipeline_options)
+
+#-----------------------------------------------------------------
 
 #Arguments:
 colunas_dengue = ['id',
@@ -70,6 +73,22 @@ def chave_uf(elemento):
     chave = elemento['uf']
     return (chave, elemento)
 
+def casos_dengue(elemento):
+    """
+    Recebe uma tupla ('RS', [{},...]) e retorna
+    uma tupla ('RS-2014-12', 8.0)
+    """
+    
+    uf, registros = elemento
+    
+    for registro in registros:
+        if bool(re.search(r'\d', registro['casos'])):
+            yield (f"{uf}-{registro['ano_mes']}", float(registro['casos']))
+        else:
+            yield (f"{uf}-{registro['ano_mes']}", 0.0)
+
+#-----------------------------------------------------------------
+
 # ***\\\Starting pipeline///*** #
 dengue = (
     pipeline
@@ -77,10 +96,10 @@ dengue = (
     | "Ler do dataset de dengue" 
         >> ReadFromText("casos_dengue.txt", skip_header_lines=1)
         
-    | "De texto para lista" 
+    | "Transforma de texto para lista" 
         >> beam.Map(texto_para_lista)
         
-    | "De lista para dicionario"
+    | "Transforma de lista para dicionario"
         >> beam.Map(lista_para_dicionario, colunas_dengue)
         
     | "Cria campo 'ano_mes'"
@@ -89,11 +108,34 @@ dengue = (
     | "Cria chave pelo estado"
         >> beam.Map(chave_uf)
         
-    | "Agrupar pelo estado (UF)"
+    | "Agrupa pelo estado (UF)"
         >> beam.GroupByKey()
+        
+    | "Descompacta casos de dengue"
+        >> beam.FlatMap(casos_dengue)
+        
+    | "Soma dos casos pela chave"
+        >> beam.CombinePerKey(sum)
         
     | "Mostra resultados"
         >> beam.Map(print)   
 )# --> PCollection
 
 pipeline.run()
+
+"""
+Anotacoes:
+    # Map: Funcao que realiza um processamento 1-to-1, ou seja, 
+      dada uma determinada funcao, uma transformacao sera 
+      realizada em um elemento e seu retorno sera um elemento 
+      processado.
+      
+      Mais info em: https://beam.apache.org/documentation/transforms/python/elementwise/map/
+      
+    # FlatMap: Funcao que realiza um processamento 1-to-many, ou seja,
+      dada uma determinada funcao, uma transformacao sera realizada em
+      um elemento, sendo este elemento iteravel ou nao; caso seja iteravel,
+      retornara um elemento processado para cada item contido.
+      
+      Mais info em: https://beam.apache.org/documentation/transforms/python/elementwise/flatmap/
+"""
